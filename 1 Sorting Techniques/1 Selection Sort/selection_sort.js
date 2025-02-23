54d90706c6e0812880e01615558fe5bdcd4af1a9
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-02-23 21:25:35 +0530

    initial commit

---
+++ 1 Sorting Techniques/1 Selection Sort/selection_sort.js
@@ -0,0 +1,24 @@
+let a = [7,5,9,2,8];
+
+console.log(a);
+
+function selectionSort(arr) {
+    for(let i=0;i<arr.length;i++){
+        //initialise the position of smallest element
+        let pos = i;
+
+        for(j=i;j<arr.length;j++){
+            //select store the position of the smallest element
+            if(arr[j]<arr[pos])
+                pos = j
+        }
+
+        //swap the positions with the smallest element
+        let temp = arr[i];
+        arr[i] = arr[pos];
+        arr[pos] = temp;
+    }
+    return arr
+}
+
+console.log(selectionSort(a));
